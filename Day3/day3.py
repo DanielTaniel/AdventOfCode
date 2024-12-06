be17d9598cb66a3d98ca0de8aa2b22bad51dbc92
DanielTan <dantan20114@gmail.com>
2024-12-06 23:28:19 +0800

    working day 3 part 2

--- Day3/day3.py
+++ Day3/day3.py
@@ -10,10 +10,10 @@ import re
 parser = argparse.ArgumentParser("AoC_Day_2")
 
 parser.add_argument("-t", "--test", help="use test data", nargs="?", const=False)
+parser.add_argument("-p", "--part", help="part 1 or 2?", type=int, default=1)
 args = vars(parser.parse_args())
 
-
-def read_and_split(test=args['test']):
+def read_data(test=args['test']):
 
     reports = []
 
@@ -26,14 +26,27 @@ def read_and_split(test=args['test']):
     
     return inputs 
 
+def strip_donts(text):
+    return re.sub(r"don\'t\(\).*?do\(\)", "[REPLACED]", text, flags=re.DOTALL)
 
-    #pprint(reports)
-#pprint(read_and_split())
-patern = r"mul\((\d{1,3}),(\d{1,3})\)"
-mult = re.findall(patern, read_and_split())
-mult = [(int(x), int(y)) for x, y in mult ]
-pprint(mult)
+def calc_mults(text):
 
-total = sum([(x*y) for x, y, in mult])
+    patern = r"mul\((\d{1,3}),(\d{1,3})\)"
+    mult = re.findall(patern, text)
+    mult = [(int(x), int(y)) for x, y in mult ]
 
-pprint(total)
+    return sum([(x*y) for x, y, in mult])
+
+def main():
+    pprint(args) 
+    if args['part'] == 1:
+        pprint(calc_mults(read_data()))
+    elif args['part'] == 2:
+        text = strip_donts(read_data())
+        pprint(text)
+        pprint(calc_mults(text))
+
+    else:
+        print("what was that? ")
+
+main()
